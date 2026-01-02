9669a9ccdee3360389f987a836b629369db9b01a
vyeos <rudranpatel0980@gmail.com>
2026-01-02 16:35:52 +0530

    edit: footer content styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,46 +19,48 @@ const Footer = () => {
         />
 
         {/* Content Overlay */}
-        <div className="absolute inset-0 z-10 flex flex-col justify-between p-8 md:p-16 text-foreground">
+        <div className="absolute inset-0 z-10 flex flex-col justify-between p-8 md:p-16 text-[#161C2D]">
           <div className="flex flex-col md:flex-row justify-between w-full h-full relative">
             {/* Left Content */}
-            <div className="flex flex-col gap-8 md:w-1/3">
+            <div className="flex flex-col gap-14 md:w-1/3">
               {/* Logo Mark */}
               <Logo />
 
-              <div className="flex flex-col gap-1 mt-auto md:mt-0 pt-20">
-                <h3 className="text-lg font-medium">Go digital with us</h3>
-                <p className="text-sm opacity-80">50 Raya Building</p>
-                <p className="text-sm opacity-80">Call us: 123-456-678</p>
+              <div className="flex flex-col gap-2">
+                <h3 className="text-3xl font-bold tracking-tight leading-tight">Elevating Digital<br />Experiences</h3>
+                <div className="flex flex-col gap-1 mt-2">
+                  <p className="text-base font-medium opacity-70">123 Innovation Drive, Tech City, CA 94043</p>
+                  <p className="text-base font-medium opacity-70">+1 (555) 123-4567</p>
+                </div>
               </div>
             </div>
 
             {/* Right Content - Links */}
             <div className="flex gap-12 md:gap-24 mt-10 md:mt-10 mr-0 md:mr-20 z-20">
               <div className="flex flex-col gap-6">
-                <h4 className="font-semibold text-lg">Company</h4>
-                <div className="flex flex-col gap-4 text-sm font-medium opacity-80">
+                <h4 className="font-bold text-sm tracking-widest uppercase opacity-40">Company</h4>
+                <div className="flex flex-col gap-4 text-base font-medium text-[#161C2D]/80">
                   <Link
                     href="/about"
-                    className="hover:opacity-100 transition-opacity"
+                    className="hover:text-[#161C2D] hover:translate-x-1 transition-all duration-300"
                   >
                     About
                   </Link>
                   <Link
                     href="/blog"
-                    className="hover:opacity-100 transition-opacity"
+                    className="hover:text-[#161C2D] hover:translate-x-1 transition-all duration-300"
                   >
                     Blog
                   </Link>
                   <Link
                     href="/gallery"
-                    className="hover:opacity-100 transition-opacity"
+                    className="hover:text-[#161C2D] hover:translate-x-1 transition-all duration-300"
                   >
                     Gallery
                   </Link>
                   <Link
                     href="/contact"
-                    className="hover:opacity-100 transition-opacity"
+                    className="hover:text-[#161C2D] hover:translate-x-1 transition-all duration-300"
                   >
                     Contact
                   </Link>
@@ -66,23 +68,23 @@ const Footer = () => {
               </div>
 
               <div className="flex flex-col gap-6">
-                <h4 className="font-semibold text-lg">Social Media</h4>
-                <div className="flex flex-col gap-4 text-sm font-medium opacity-80">
+                <h4 className="font-bold text-sm tracking-widest uppercase opacity-40">Social Media</h4>
+                <div className="flex flex-col gap-4 text-base font-medium text-[#161C2D]/80">
                   <Link
                     href="#"
-                    className="hover:opacity-100 transition-opacity"
+                    className="hover:text-[#161C2D] hover:translate-x-1 transition-all duration-300"
                   >
                     Facebook
                   </Link>
                   <Link
                     href="#"
-                    className="hover:opacity-100 transition-opacity"
+                    className="hover:text-[#161C2D] hover:translate-x-1 transition-all duration-300"
                   >
                     Instagram
                   </Link>
                   <Link
                     href="#"
-                    className="hover:opacity-100 transition-opacity"
+                    className="hover:text-[#161C2D] hover:translate-x-1 transition-all duration-300"
                   >
                     Twitter
                   </Link>
@@ -91,8 +93,8 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="mt-8 relative z-20">
-            <p className="text-sm opacity-60">
+          <div className="w-1/3 mt-8 relative z-20 border-t border-[#161C2D] pt-8">
+            <p className="text-sm font-medium">
               Copyright © 2025 Vision & Path. All Rights Reserved.
             </p>
           </div>
