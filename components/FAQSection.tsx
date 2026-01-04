0efbf44365d908975d32ee04f174af1a1345ecdc
vyeos <rudranpatel0980@gmail.com>
2026-01-05 02:24:41 +0530

    fix: faq resp

--- components/FAQSection.tsx
+++ components/FAQSection.tsx
@@ -18,7 +18,7 @@ const FAQSection = () => {
   };
 
   return (
-    <div className="min-h-screen">
+    <div className="py-20">
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
