fedd558a7624efacf1fd647a6feee957e90e0fa9
Sahil-796 <sp0709006@gmail.com>
2026-01-21 21:13:19 +0530

    fix: www

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.visionandpath.com";
+  const baseUrl = "https://visionandpath.com";
 
   return [
     {
